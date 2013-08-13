66365c5449ffd04b25a479c67c5639ccd9bf42dd
Raoul Jean Pierre Bonnal <ilpuccio.febo@gmail.com>
2013-08-14 00:57:45 +0200

    defines reference server for PBS operations and basic methods for environment configuration

--- lib/torque_rm/common.rb
+++ lib/torque_rm/common.rb
@@ -1,3 +1,28 @@
+require 'rye'
 module TORQUE
+  @@qcommands_path = '/usr/bin'
+  @@master = nil
+  def self.server=(hostname=nil)
+  	if hostname.nil?
+  		@@master = Rye.shell
+  	else
+  		@@master = Rye::Box.new(hostname)
+  	end
+  end
 
+  def self.server
+  	@@master
+  end
+
+  def self.qcommands_path=(path)
+  	@@qcommands_path = path
+  	%w(qstat qsub qdel).each do |command|
+  	  Rye::Cmd.remove_command command if Rye::Cmd.can? command
+  	  Rye::Cmd.add_command command, File.join(qcommands_path,'qstat')
+  	end
+  end
+
+  def self.qcommands_path
+  	@@qcommands_path
+  end
 end
